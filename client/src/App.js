634bfd55f6d3259ada152aec0bde9c677e15135f
Andy Rallis <andy.rallis@rallisfamily.com>
2021-11-01 18:46:08 -0600

    Test change push

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Fib Values Calculator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
